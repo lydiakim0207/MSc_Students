edf8a9bc76bf7f5220120d1d97e6a919c7c59b67
lydiakim0207 <84908772+lydiakim0207@users.noreply.github.com>
2021-05-27 11:48:57 +0100

    Update lydia.sh
    i have attached the current code we used to produce the empty .samm file

--- lydia.sh
+++ lydia.sh
@@ -25,3 +25,38 @@ metaphlan  $SAMP/metagenome_1.fastq,$SAMP/metagenome_2.fastq --bowtie2out $DATAB
 
  
 echo "$dt"
+
+
+
+
+
+#SBATCH --time=6:00:00
+
+
+
+conda activate microbiome
+
+module load apps/bowtie2/2.3.5.1
+
+
+DATABASE="/users/k2038644/.conda/envs/project2021/lib/python3.7/site-packages/metaphlan/metaphlan_databases"
+
+SAMP="/users/k2038644/Trimmed/Sample_10-015-Group-1"
+
+RESULTS="/users/k2038644/Trimmed/Trimmed.Results"
+
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+
+echo "$dt"
+
+
+metaphlan $SAMP/10-015-Group-1_210224_L002_R0.fastq --input_type fastq $DATABASE mpa_v30_CHOCOPhlAn_201901 > $RESULTS/Healthy.Sample10.sam
+
+
+echo "$dt"
+
+
+
+
+
